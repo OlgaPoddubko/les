c043e39dfe42e40c653049b239b35c8bbfbb2f49
Olga <olgapoddubko@gmail.com>
2018-06-10 16:54:46 +0300

    2_10

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,3 +1,24 @@
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed; false,
+      };
+    case 'TOGGLE_TODO':
+      if (state.is !== action.id) {
+        return state;
+      }
+      return {
+        ...state,
+        completed: !state.completed,
+      };
+    default:
+      return state;
+  }
+};
+
 const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
@@ -6,8 +27,25 @@ const todos = (state = [], action) => {
         todo(undefined, action)
       ];
     case 'TOGGLE_TODO':
-      return state.map(t => todo(t, action));
+      return state.map(t =>
+        todo(t, action)
+      );
     default:
       return state;
+  }
+};
+
+export default todos;
+
+export const getVisibleTodos = (state, filter ) => {
+  switch (filter) {
+  case 'all':
+    return state;
+  case 'completed':
+    return state.filter( t => t.completed );
+  case 'active':
+    return state.filter( t => !t.completed );
+  default:
+    throw new Error(`Unknown filter: ${filter}.`);
   }
 };
