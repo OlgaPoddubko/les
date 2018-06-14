f6f58dfb3f738ef9ae656947d9dff95ac76298dc
Olga <olgapoddubko@gmail.com>
2018-06-14 17:44:29 +0300

    2_22

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,10 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
-import promise from 'redux-promise';
 import createLogger from 'redux-logger';
 import todoApp from './reducesrs';
 
+const thunk = (store) => (next) => (action) =>
+  typeof action === 'function' ?
+    action(store.dispatch) :
+    next(action);
+
 const configureStore = () => {
-  const middlewares = [promise];
+  const middlewares = [thunk];
   if (process.env.NODE_ENV !== 'production') {
     middlewares.push(createLogger());
   }
@@ -12,7 +16,7 @@ const configureStore = () => {
   return createStore(
     todoApp,
     //persistedState, // if needed
-    applyMiddleware(...middlewares)s
+    applyMiddleware(...middlewares),
   );
 };
 
